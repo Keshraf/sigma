dbb0977575da7a477bf6e56aaa0a128580290b1f
Ketan Saraf <saraf.ketan1108@gmail.com>
2022-09-19 06:00:33 +0530

    Removed Fonts API

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-  env: {
-    FONTS_API_KEY: "AIzaSyB94cIrlL_xf6xd2N88Op2c87fKDnMHNis",
-  },
 };
 
 module.exports = nextConfig;
