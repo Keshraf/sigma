5a082469edf94042ee7bfc4d7a676b8f2ee91bb2
Ketan Saraf <saraf.ketan1108@gmail.com>
2022-09-30 18:51:39 +0530

    Added Authentication Page

--- store/roomSlice.js
+++ store/roomSlice.js
@@ -3,12 +3,22 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   id: "",
   pages: 1,
+  admin: "",
+  name: "",
 };
 
 export const roomSlice = createSlice({
   name: "room",
   initialState,
   reducers: {
+    setInitialRoom(state, action) {
+      return {
+        id: action.payload.id,
+        admin: action.payload.admin,
+        name: action.payload.name,
+        pages: action.payload.pages,
+      };
+    },
     setRoom(state, action) {
       state.id = action.payload.id;
     },
@@ -18,8 +28,17 @@ export const roomSlice = createSlice({
     setPageRoom(state, action) {
       state.pages = action.payload.pages;
     },
+    setRoomName(state, action) {
+      state.name = action.payload.name;
+    },
   },
 });
 
-export const { setRoom, addPageRoom, setPageRoom } = roomSlice.actions;
+export const {
+  setRoom,
+  addPageRoom,
+  setPageRoom,
+  setInitialRoom,
+  setRoomName,
+} = roomSlice.actions;
 export default roomSlice.reducer;
